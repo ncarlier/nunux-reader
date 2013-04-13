1e6b93d063cf25ad6885340496f8e156be6e2016
Nicolas Carlier <nicolas.carlier@atos.net>
2013-04-14 00:33:50 +0200

    Fix tab/spaces

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,42 +1,42 @@
 $(function() {
-	var $articles = $('#articles');
-	var $articleTmpl = $('#article-tmpl').find(">:first-child");
-	var params = {
-		offset: 0,
-		size: 10
-	}
-	var getArticles = function() {
-		$.getJSON('article', params)
-		.done(function(articles) {
-			$.each(articles, function(i, article) {
-				var $article = $articleTmpl.clone();
-				$('header a', $article).text(article.title).attr('href', article.link);
-				$('header span', $article).text(moment(article.date).format("dddd, MMMM Do YYYY, h:mm:ss"));
-				$('p', $article).html(article.description);
-				$article.appendTo($articles);
-				console.log('Add article ' + article.guid);
-			});
-		})
-		.fail(function(jqxhr, textStatus, error ) {
-			var err = textStatus + ', ' + error;
-			console.log( "Request Failed: " + err);
-		});
-	}
+  var $articles = $('#articles');
+  var $articleTmpl = $('#article-tmpl').find(">:first-child");
+  var params = {
+    offset: 0,
+    size: 10
+  }
+  var getArticles = function() {
+    $.getJSON('article', params)
+    .done(function(articles) {
+      $.each(articles, function(i, article) {
+        var $article = $articleTmpl.clone();
+        $('header a', $article).text(article.title).attr('href', article.link);
+        $('header span', $article).text(moment(article.date).format("dddd, MMMM Do YYYY, h:mm:ss"));
+        $('p', $article).html(article.description);
+        $article.appendTo($articles);
+        console.log('Add article ' + article.guid);
+      });
+    })
+    .fail(function(jqxhr, textStatus, error ) {
+      var err = textStatus + ', ' + error;
+      console.log( "Request Failed: " + err);
+    });
+  }
 
-	getArticles();
-	$(window).scroll(function() {
-		if ($(window).scrollTop() == ($(document).height() - $(window).height())) {
-			params.offset += params.size;
-			getArticles();
-		}
-		var areaHeight = $(this).height();
+  getArticles();
+  $(window).scroll(function() {
+    if ($(window).scrollTop() == ($(document).height() - $(window).height())) {
+      params.offset += params.size;
+      getArticles();
+    }
+    var areaHeight = $(this).height();
 
-		$('#articles article').each(function() {
-			var top = $(this).position().top;
-			var height = $(this).height();
+    $('#articles article').each(function() {
+      var top = $(this).position().top;
+      var height = $(this).height();
 
-			var visible = !(top + height < 0 || top > areaHeight);
-			$(this).toggleClass('visible', visible);
-		});
-	});
+      var visible = !(top + height < 0 || top > areaHeight);
+      $(this).toggleClass('visible', visible);
+    });
+  });
 });
--- routes/article.js
+++ routes/article.js
@@ -11,20 +11,20 @@ db.on('error', function (err) {
  * GET article listing.
  */
 exports.list = function(req, res, next) {
-	console.log('query: %j', req.query);
-	var getArticle = function(key, callback) {
-		db.get(key, function(err, article) {
-			if (err) return callback(err);
-			callback(null, JSON.parse(article));
-		});
-	}
-	var key = 'user:nicolas@nunux.org:playlist';
-	//ZRANGEBYSCORE myzset -inf +inf
-	db.zrangebyscore(key, '-inf', '+inf', 'LIMIT', req.query.offset, '10', function(err, replies) {
-		if (err) return next(err);
-		async.map(replies, getArticle, function(err, results){
-			if (err) return next(err);
-			res.json(results);
-		});
-	});
+  console.log('query: %j', req.query);
+  var getArticle = function(key, callback) {
+    db.get(key, function(err, article) {
+      if (err) return callback(err);
+      callback(null, JSON.parse(article));
+    });
+  }
+  var key = 'user:nicolas@nunux.org:playlist';
+  //ZRANGEBYSCORE myzset -inf +inf
+  db.zrangebyscore(key, '-inf', '+inf', 'LIMIT', req.query.offset, '10', function(err, replies) {
+    if (err) return next(err);
+    async.map(replies, getArticle, function(err, results){
+      if (err) return next(err);
+      res.json(results);
+    });
+  });
 };
