da4fccb53e5ee58d2fcd8ac6e4b70d6a7f7a7719
Nicolas Carlier <n.carlier@nunux.org>
2013-05-22 17:45:01 +0200

    Improve bottom page detection and fix visibility detection for mobile viewport

--- public/javascripts/views/timeline.js
+++ public/javascripts/views/timeline.js
@@ -6,40 +6,6 @@ define([
        'channel',
        'text!templates/article.html'
 ], function($, _, Backbone, moment, channel, articleTpl){
-  var keepUnreadHandler = function() {
-    var aid = $(this).attr('id').split('/')[0];
-    var parts = aid.split(':');
-    var timeline = aid[0] + ':' + aid[1];
-    if ($(this).is(':checked')) {
-      $.ajax({
-        url: 'timeline/' + timeline + '/' + aid,
-        type: 'PUT',
-        dataType: 'json',
-        success: function(res) {
-          $(this).parents('article').addClass('keep');
-          res.timeline = timeline;
-          channel.trigger('app.event.timelinesize', res);
-        }.bind(this)
-      });
-    } else {
-      $(this).parents('article').removeClass('keep seen confirm');
-    }
-  }
-
-  var saveThisHandler = function() {
-    var id = $(this).attr('id').split('/')[0];
-    var type = $(this).is(':checked') ? 'PUT' : 'DELETE';
-    $.ajax({
-      url: 'timeline/archive/' + id,
-      type: type,
-      dataType: 'json',
-      success: function(res) {
-        res.timeline = 'archive';
-        channel.trigger('app.event.timelinesize', res);
-      }
-    });
-  }
-
   var cleanArticleContent = function($article, meta) {
       //$('script', $article).not('script[src^="http://www.youtube"]').remove();
       $('script', $article).filter('script[src^="http://feeds.feedburner.com"]').remove();
@@ -51,6 +17,26 @@ define([
         }
       });
       return $article;
+  }
+
+  var elementInViewport = function(el) {
+    var top = el.offsetTop;
+    var left = el.offsetLeft;
+    var width = el.offsetWidth;
+    var height = el.offsetHeight;
+
+    while(el.offsetParent) {
+      el = el.offsetParent;
+      top += el.offsetTop;
+      left += el.offsetLeft;
+    }
+
+    return (
+      top < (window.pageYOffset + window.innerHeight) &&
+      left < (window.pageXOffset + window.innerWidth) &&
+      (top + height) > window.pageYOffset &&
+      (left + width) > window.pageXOffset
+    );
   }
 
   return Backbone.View.extend({
@@ -68,8 +54,9 @@ define([
     render: function() {
       this.$articles = this.$el;
       this.$articles.scroll(this.handleScrollEvent.bind(this));
-      this.$articles.delegate('footer input.keep', 'change', keepUnreadHandler);
-      this.$articles.delegate('footer input.save', 'change', saveThisHandler);
+      $(document).scroll(this.handleScrollEvent.bind(this));
+      this.$articles.delegate('footer input.keep', 'change', this.keepUnreadHandler.bind(this));
+      this.$articles.delegate('footer input.save', 'change', this.saveThisHandler.bind(this));
       this.fetchTimeline();
       channel.trigger('app.event.timelinechange', {timeline: this.options.timeline});
     },
@@ -130,36 +117,70 @@ define([
       }.bind(this));
     },
 
-    handleScrollEvent: function() {
-      if (this.$articles.scrollTop() + this.$articles.height() > this.$articles[0].scrollHeight - 100) {
+    handleScrollEvent: function(event) {
+      if (this.$articles.children('.not-seen').length <= 1) {
+        //if (this.$articles.scrollTop() + this.$articles.height() > this.$articles[0].scrollHeight - 100) {
         this.fetchTimeline();
       }
-      if (this.isReadable()) this.updateSeenArticles();
+      if (this.isReadable()) this.updateSeenArticles(event);
     },
 
-    updateSeenArticles: function() {
+    updateSeenArticles: function(event) {
       var areaHeight = this.$el.height();
       var timeline = this.options.timeline;
       var timelineUrl = this.getTimelineUrl();
+      var isVisible = (event.target == document) ? elementInViewport : function(el) {
+        var top = $(el).position().top;
+        var height = $(el).height();
+        return !(top + height < 0 || top > areaHeight);
+      };
 
-      $('article', this.$el).each(function() {
-        var top = $(this).position().top;
-        var height = $(this).height();
-
-        var visible = !(top + height < 0 || top > areaHeight);
-        if (visible && !$(this).hasClass('seen') && !$(this).hasClass('keep')) {
-          $(this).addClass('seen');
+      $('article.not-seen', this.$el).each(function() {
+        if (isVisible(this)) {
+          $(this).removeClass('not-seen').addClass('seen');
           // TODO group ajax calls in a buffered one
           $.ajax({
             url: timelineUrl + '/' + $(this).attr('id'),
             type: 'DELETE',
             dataType: 'json',
             success: function(res) {
-              $(this).addClass('confirm');
               res.timeline = timeline;
               channel.trigger('app.event.timelinesize', res);
             }.bind(this)
           });
+        }
+      });
+    },
+
+    keepUnreadHandler: function(event) {
+      var $checkbox = $(event.target);
+      var aid = $checkbox.attr('id').split('/')[0];
+      var type = $checkbox.is(':checked') ? 'PUT' : 'DELETE';
+      var css = $checkbox.is(':checked') ? 'kept-not-seen' : 'seen';
+      var timeline = this.options.timeline;
+      $.ajax({
+        url: 'timeline/' + timeline + '/' + aid,
+        type: type,
+        dataType: 'json',
+        success: function(res) {
+          $checkbox.parents('article').attr('class', css);
+          res.timeline = timeline;
+          channel.trigger('app.event.timelinesize', res);
+        }
+      });
+    },
+
+    saveThisHandler: function(event) {
+      var $checkbox = $(event.target);
+      var aid = $checkbox.attr('id').split('/')[0];
+      var type = $checkbox.is(':checked') ? 'PUT' : 'DELETE';
+      $.ajax({
+        url: 'timeline/archive/' + aid,
+        type: type,
+        dataType: 'json',
+        success: function(res) {
+          res.timeline = 'archive';
+          channel.trigger('app.event.timelinesize', res);
         }
       });
     }
