37009b5a8a42f2027c2c846a58b8883c4f6946c0
Nicolas Carlier <n.carlier@nunux.org>
2013-08-08 13:43:59 +0200

    Improve tests

---
+++ tests/article.js
@@ -0,0 +1,116 @@
+require('date-utils');
+var should = require('should'),
+    path = require('path'),
+    db = require('../lib/db'),
+    logger = require('../lib/logger'),
+    Article = require('../lib/article');
+
+logger.setLevel('info');
+db.select(9);
+
+describe('An new article', function() {
+  var uid = 'user:foo@bar.com';
+  var fid = 'feed:test';
+  var aid = fid + ':e133a7b26a7701b1d65a61683e50512b';
+  var article = {
+    date: new Date(),
+    pubdate: new Date(),
+    title: 'Test article',
+    link: 'http://foo.bar',
+    description: '...',
+    badAttribute: 'evil is my name',
+    meta: {
+    }
+  };
+
+  before(function(done) {
+    db.flushdb(done);
+  });
+
+  it('should be created and clean', function(done) {
+    Article.create(article, {id: fid}, function(err, art) {
+      if (err) return done(err);
+      art.id.should.equal(aid);
+      should.not.exist(art.badAttribute);
+      Article.assertKey(art.id).should.be.ok;
+      done();
+    });
+  });
+  it('but not twice', function(done) {
+    Article.create(article, {id: fid}, function(err, art) {
+      err.should.equal('EEXIST');
+      should.not.exist(art);
+      done();
+    });
+  });
+  it('should be retrieve', function(done) {
+    Article.get(aid, function(err, art) {
+      if (err) return done(err);
+      art.id.should.equal(aid);
+      art.fid.should.equal(fid);
+      done();
+    });
+  });
+  it('should be copied', function(done) {
+    var target = uid + ':' + aid;
+    Article.copy(aid, target, function(err, art) {
+      if (err) return done(err);
+      art.id.should.equal(target);
+      art.fid.should.equal(fid);
+      done();
+    });
+  });
+  it('should be deleted (the copy)', function(done) {
+    var target = uid + ':' + aid;
+    Article.del(target, function(err) {
+      if (err) return done(err);
+      done();
+    });
+  });
+  it('and should not be retrieve (the copy)', function(done) {
+    var target = uid + ':' + aid;
+    Article.get(target, function(err, art) {
+      err.should.equal('ENOTFOUND');
+      should.not.exist(art);
+      done();
+    });
+  });
+});
+
+describe('An bad article', function() {
+  var fid = 'feed:test';
+  var aid = fid + ':cb0b35ab1c8d3301769a89258ceb1481';
+  var article = {
+    date: new Date(),
+    pubdate: new Date(),
+    link: 'http://foo.bar/bad',
+    description: '...',
+    meta: {
+    }
+  };
+
+  it('should not be created because missing attribute', function(done) {
+    Article.create(article, {id: fid}, function(err, art) {
+      err.should.equal('EATTRIBUTE');
+      should.not.exist(art);
+      done();
+    });
+  });
+  it('should not be created because too old', function(done) {
+    article.title = "Foo title";
+    article.pubdate.addDays(-300);
+    Article.create(article, {id: fid}, function(err, art) {
+      err.should.equal('ETOOOLD');
+      should.not.exist(art);
+      done();
+    });
+  });
+  it('and should not be retrieve', function(done) {
+    Article.get(aid, function(err, art) {
+      err.should.equal('ENOTFOUND');
+      should.not.exist(art);
+      done();
+    });
+  });
+});
+
--- tests/user.js
+++ tests/user.js
@@ -2,26 +2,44 @@ var should = require('should'),
     path = require('path'),
     db = require('../lib/db'),
     logger = require('../lib/logger'),
+    Article = require('../lib/article');
+    Feed = require('../lib/feed');
     User = require('../lib/user');
 
 logger.setLevel('info');
 db.select(9);
 
-describe('A new User', function() {
-  var user,
-      uid = 'test@test.com',
-      url = 'http://feeds.feedburner.com/KorbensBlog-UpgradeYourMind',
-      fid = 'feed:4d32abddd88fd326673004a92b434f94',
+describe('A new user', function() {
+  var uid = 'foo@bar.com',
+      feed = {
+        xmlurl: 'http://feeds.feedburner.com/KorbensBlog-UpgradeYourMind'
+      },
+      article = {
+        date: new Date(),
+        pubdate: new Date(),
+        title: 'Foo article',
+        link: 'http://foo.bar/new',
+        description: '...',
+        meta: {
+        }
+      },
       file = path.join(__dirname, 'data', 'subscriptions.xml');
   before(function(done) {
-    User.create({uid: uid} , function(err,u) {
-      user = u;
-      done();
+    Feed.create(feed, function(err, f) {
+      if (err) return done(err);
+      feed = f;
+      Article.create(article, feed, function(err, a) {
+        article = a;
+        done(err);
+      });
     });
   });
-
-  it('should have a registration date', function() {
-    user.should.have.property('registrationDate');
+  it('should be created', function(done) {
+    User.create({uid: uid}, function(err, user) {
+      if (err) return done(err);
+      user.should.have.property('registrationDate');
+      done();
+    });
   });
   it('should then exist', function(done) {
     User.exists(uid, function(err, exists) {
@@ -31,21 +49,21 @@ describe('A new User', function() {
     });
   });
   it('should be retrieve', function(done) {
-    User.find(uid, function(err, u) {
+    User.find(uid, function(err, user) {
       if (err) return done(err);
-      u.uid.should.equal(uid);
+      user.uid.should.equal(uid);
       done();
     });
   });
   it('should be able to subscribe a feed', function(done) {
-    User.subscribe(uid, url, function(err, feed) {
+    User.subscribe(uid, feed.xmlurl, function(err, f) {
       if (err) return done(err);
-      feed.xmlurl.should.equal(url);
+      f.xmlurl.should.equal(feed.xmlurl);
       done();
     });
   });
   it('should import OPML file', function(done) {
-    User.import(uid, file, function(err) {
+    User.importSubscriptions(uid, file, function(err) {
       if (err) return done(err);
       User.getSubscriptions(uid, function(err, feeds) {
         if (err) return done(err);
@@ -55,8 +73,62 @@ describe('A new User', function() {
       });
     });
   });
+  it('should have a global timeline', function(done) {
+    User.getTimelineStatus(uid, 'global', function(err, status) {
+      if (err) return done(err);
+      status.title.should.equal('All items');
+      status.size.should.equal(1);
+      status.should.not.have.property('feed');
+      done();
+    });
+  });
+  it('should have a feed timeline', function(done) {
+    User.getTimelineStatus(uid, feed.id, function(err, status) {
+      if (err) return done(err);
+      status.should.have.property('feed');
+      status.feed.id.should.equal(feed.id);
+      status.size.should.equal(1);
+      done();
+    });
+  });
+  it('should get content of the feed timeline', function(done) {
+    var options = {};
+    User.getTimeline(uid, feed.id, options, function(err, result) {
+      if (err) return done(err);
+      result.should.not.have.property('next');
+      result.should.not.have.property('order');
+      result.articles.should.not.be.empty;
+      result.articles[0].link.should.equal(article.link);
+      done();
+    });
+  });
+  it('should save an article into the archive timeline', function(done) {
+    User.addArticleToTimeline(uid, 'archive', article.id, function(err, status) {
+      if (err) return done(err);
+      status.title.should.equal('Saved items');
+      status.size.should.equal(1);
+      done();
+    });
+  });
+  it('should remove an article from the feed timeline', function(done) {
+    User.removeArticleFromTimeline(uid, feed.id, article.id, function(err, status) {
+      if (err) return done(err);
+      status.should.have.property('feed');
+      status.feed.id.should.equal(feed.id);
+      status.size.should.equal(0);
+      done();
+    });
+  });
+  it('should have no more article in global timeline', function(done) {
+    User.getTimelineStatus(uid, 'global', function(err, status) {
+      if (err) return done(err);
+      status.title.should.equal('All items');
+      status.size.should.equal(0);
+      done();
+    });
+  });
   it('should be able to un-subscribe a feed', function(done) {
-    User.unSubscribe(uid, fid, function(err) {
+    User.unSubscribe(uid, feed.id, function(err) {
       if (err) return done(err);
       done();
     });
@@ -66,24 +138,6 @@ describe('A new User', function() {
       if (err) return done(err);
       feeds.should.not.be.empty;
       feeds.should.have.length(1);
-      done();
-    });
-  });
-  it('should have a global timeline', function(done) {
-    User.getTimelineStatus(uid, 'global', function(err, status) {
-      if (err) return done(err);
-      status.title.should.equal('All items');
-      status.size.should.equal(0);
-      status.should.not.have.property('feed');
-      done();
-    });
-  });
-  it('should have a feed timeline', function(done) {
-    User.getTimelineStatus(uid, fid, function(err, status) {
-      if (err) return done(err);
-      status.should.have.property('feed');
-      status.feed.id.should.equal(fid);
-      status.size.should.equal(0);
       done();
     });
   });
