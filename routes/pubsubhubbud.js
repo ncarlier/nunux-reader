7553a1d769acee6723e4148b0eba841838a2c52d
Nicolas Carlier <n.carlier@nunux.org>
2013-10-09 10:05:10 +0200

    fix

--- routes/pubsubhubbud.js
+++ routes/pubsubhubbud.js
@@ -53,8 +53,8 @@ module.exports = function(app){
   app.post('/pubsubhubbud/callback', app.ensurePubSubHubBud, function(req, res, next) {
     Feed.updateArticles(req.body, null, function(err) {
       if (err) {
+        logger.error('PSHB REQUEST: %s', err);
         logger.error('PSHB REQUEST: %j', req.body);
-        return next(err);
       }
       res.send(200);
     });
