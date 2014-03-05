becde5209ee836de4eb60448552e95464fa23e21
Nicolas Carlier <n.carlier@nunux.org>
2014-03-05 19:55:21 +0100

    Add dump api

--- server/routes/user.js
+++ server/routes/user.js
@@ -16,4 +16,20 @@ module.exports = function(app) {
       res.json(result);
     });
   });
+
+  /**
+   * GET user saved articles.
+   */
+  app.get('/api/user/:uid/dump', app.ensureAuthenticated, function(req, res, next) {
+    if (req.params.uid !== req.user.uid) {
+      return next(new errors.Forbidden());
+    }
+    var file = '/tmp/reader-'  + req.user.uid + '.json';
+    User.exportArticles(req.user.uid, file, function(err) {
+      if (err) {
+        return next(err);
+      }
+      res.download(file, 'dump.json');
+    });
+  });
 };
