823778a98b3502e4f2a28d0b2de89fec22098195
Nicolas Carlier <n.carlier@nunux.org>
2016-12-11 21:21:28 +0100

    feat(): Add PWA manifest.

--- grunt/copy.js
+++ grunt/copy.js
@@ -5,7 +5,8 @@ module.exports = {
       {expand: true, cwd: 'client/', src: ['icons/**'], dest: 'dist/'},
       {expand: true, cwd: 'client/', src: ['images/**'], dest: 'dist/'},
       {expand: true, cwd: 'client/', src: ['templates/**'], dest: 'dist/'},
-      {expand: true, cwd: 'client/', src: ['robots.txt'], dest: 'dist/'}
+      {expand: true, cwd: 'client/', src: ['robots.txt'], dest: 'dist/'},
+      {expand: true, cwd: 'client/', src: ['manifest.json'], dest: 'dist/'}
     ]
   }
 };
